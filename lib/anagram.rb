a5392ecf44ea453dcac0dab7d02729b6c3891b1e
Sharon Awuor <sawuor395@gmail.com>
2023-07-07 14:35:54 +0300

    completed

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,14 @@
 # Your code goes here!
+class Anagram
+    attr_reader :word
+    
+    def initialize(word)
+        @word = word
+    end
+
+    def match(anagrams)
+        anagrams.select do |anagram|
+          anagram.downcase != word && anagram.downcase.chars.sort == word.chars.sort
+        end
+      end
+end
